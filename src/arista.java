7749a48e6be9927bafc9c672c189b45b8c9eb7a4
Sammy Guevara <sammygm6@gmail.com>
2015-09-12 19:44:43 -0600

    Creando clase Arista

--- src/arista.java
+++ src/arista.java
@@ -1,13 +1,45 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author User
- */
 public class arista {
+    private int peso;
+    private vertice anterior;
+    private vertice siguiente;
 
+    public arista() {
+    }
+
+    public arista(int peso, vertice anterior, vertice siguiente) {
+        this.peso = peso;
+        this.anterior = anterior;
+        this.siguiente = siguiente;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+
+    public vertice getAnterior() {
+        return anterior;
+    }
+
+    public void setAnterior(vertice anterior) {
+        this.anterior = anterior;
+    }
+
+    public vertice getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(vertice siguiente) {
+        this.siguiente = siguiente;
+    }
+
+    @Override
+    public String toString() {
+        return Integer.toString(peso);
+    }
+    
+    
 }
