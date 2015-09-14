4e521f1d90f5a6090870be021c7c31970fed2cd8
Sammy Guevara <sammygm6@gmail.com>
2015-09-14 09:28:33 -0600

    creando un filechooser y un dialogo para ya iniciar a dibujar los planetas y las aristas, ahorita tratare de hacer el codigo para hacerlo dinamicamente

--- src/Main.java
+++ src/Main.java
@@ -26,17 +26,28 @@ public class Main extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jfc_CargarGalaxia = new javax.swing.JFileChooser();
+        jButton1 = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jButton1.setText("Cargar Galaxia");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(133, 133, 133)
+                .addComponent(jButton1)
+                .addContainerGap(164, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(77, 77, 77)
+                .addComponent(jButton1)
+                .addContainerGap(200, Short.MAX_VALUE))
         );
 
         pack();
@@ -78,5 +89,7 @@ public class Main extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JFileChooser jfc_CargarGalaxia;
     // End of variables declaration//GEN-END:variables
 }
